c8bc8b3d561c7e70b80c48a7dd6ddca230c2411c
Prekaya <ukabapan@gmail.com>
2022-02-14 17:29:08 +0000

    Welp I tried

--- scripts/dynamic_tf_cam_publisher.py
+++ scripts/dynamic_tf_cam_publisher.py
@@ -1,33 +1,58 @@
 #!/usr/bin/env python
 import rospy
-import tf
-import tf2_msgs
+
+import tf_conversions
+import math
 import tf2_ros
-import numpy as np
 import geometry_msgs.msg
-from std_msgs.msg import Float32
 
 
-    
 def dynamic_tf_cam_publisher():
     rospy.init_node('dynamic_tf_cam_publisher', anonymous=False)
-    br_left = tf.TransformBroadcaster()
-    br_right = tf.TransformBroadcaster()
+    br_left = tf2_ros.TransformBroadcaster()
+    br_right = tf2_ros.TransformBroadcaster()
     tfBuffer = tf2_ros.Buffer()
-    listener = tf.TransformListener(tfBuffer)
+    listener = tf2_ros.TransformListener(tfBuffer)
+
+    br_left = tf2_ros.TransformBroadcaster()
+    t_left = geometry_msgs.msg.TransformStamped()
+
+    t_left.header.stamp = rospy.Time.now()   
+    t_left.header.frame_id = "base_link_get"
+    t_left.child_frame_id = "left_cam"
+    t_left.transform.translation.x = 0.0
+    t_left.transform.translation.y = -0.05
+    t_left.transform.translation.z = 0.0
+    q_left = tf_conversions.transformations.quaternion_from_euler(0, 0, 0)
+    t_left.transform.rotation.x = q_left[0]
+    t_left.transform.rotation.y = q_left[1]
+    t_left.transform.rotation.z = q_left[2]
+    t_left.transform.rotation.w = q_left[3]
+
+    br_right = tf2_ros.TransformBroadcaster()
+    t_right = geometry_msgs.msg.TransformStamped()
+
+    t_right.header.stamp = rospy.Time.now()   
+    t_right.header.frame_id = "left_cam"
+    t_right.child_frame_id = "right_cam"
+    t_right.transform.translation.x = 0.0
+    t_right.transform.translation.y = 0.1
+    t_right.transform.translation.z = 0.0
+    q_right = tf_conversions.transformations.quaternion_from_euler(0, 0, 0)
+    t_right.transform.rotation.x = q_right[0]
+    t_right.transform.rotation.y = q_right[1]
+    t_right.transform.rotation.z = q_right[2]
+    t_right.transform.rotation.w = q_right[3]
     
     while not rospy.is_shutdown():
-        br_left.sendTransform((0.0, -0.05, 0.0),(0.0, 0.0, 0.0, 1.0),rospy.Time.now(),
-                            "left_cam",
-                            "base_link_get")
-        br_right.sendTransform((0.0, 0.05, 0.0),(0.0, 0.0, 0.0, 1.0),rospy.Time.now(),
-                            "right_cam",
-                            "base_link_get")
+        br_left.sendTransform(t_left)
+        br_right.sendTransform(t_right)
         try:
-            (t_bot, r_bot) = listener.lookupTransform('/base_link_get', '/world', rospy.Time.now())
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            t_bot = tfBuffer.lookupTransform('/base_link_get', '/world', rospy.Time())
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            rate.sleep()
             continue
-        rospy.loginfo(5.0)
+        rospy.loginfo(t_bot)
         ##transform_right = listener.lookupTransform('/right_cam', '/base_link_get', rospy.Time())
 
 
